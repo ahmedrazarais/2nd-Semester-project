bc51798129ce37349f8ee95697b8d28d9a606037
ahmedrazarais <razarais28@gmail.com>
2024-07-08 20:12:30 +0500

    Connetion Credentials for mysql

---
+++ sqlconnection.py
@@ -0,0 +1,6 @@
+# There are variables that must be set before starting main project.Must set according
+# to your system configuration
+
+database_host="your_host"
+database_user="your_user"
+database_password="your password"  # change these with yours
